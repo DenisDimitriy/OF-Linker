a603696c246a8204a926a2fc79b1669118eb7e00
DenisDimitriy <denis.dimitriy@gmail.com>
2019-04-07 22:11:26 +0200

    add data and my-lib

---
+++ data.js
@@ -0,0 +1,87 @@
+zone19 = {
+    name: "Зона 19",
+    content: [
+        box19_26
+    ]
+};
+
+var box19_26 = {
+    name: "АРШ 19-26",
+    container: zone19,
+    content: [
+        odf1,
+        odf2,
+        odf3,
+        odf4,
+    ]
+};
+var box17_01 = {
+    name: "АРШ 17-01",
+    container: zone19,
+    content: [
+        odf1,
+        odf2,
+        odf3,
+        odf4,
+    ]
+};
+
+var odf1 = {
+    name: "ODF №1",
+    container: box19_26,
+    content: [
+        "Ощадбанк",
+        "Аваль",
+        "ОБК",
+        "",
+        "",
+        "",
+        "",
+        "",
+    ]
+};
+
+var odf2 = {
+    name: "ODF №2",
+    container: box19_26,
+    content: [
+        "ICC",
+        "PrivatBank",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+    ]
+};
+
+var odf3 = {
+    name: "ODF №3",
+    container: box19_26,
+    content: [
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+    ]
+};
+
+var odf4 = {
+    name: "ODF №4",
+    container: box19_26,
+    content: [
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+    ]
+};
